e535bf219d76adfcc0f61be802e3f277d6683683
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-03-27 10:33:53 -0600

    Added types

---
+++ type.cpp
@@ -0,0 +1,17 @@
+#include "type.hpp"
+
+int Type::size(){
+    return 4;
+}
+
+int Integer::size(){
+    return 4;
+}
+
+int Character::size(){
+    return 1;
+}
+
+int CharacterString::size(){
+    return 0;
+}
---
+++ type.hpp
@@ -0,0 +1,21 @@
+class Type{
+public:
+    virtual int size();
+private:
+protected:
+};
+
+class Integer{
+public:
+    int size();
+};
+
+class Character{
+public:
+    int size();
+};
+
+class CharacterString{
+public:
+    int size();
+};
