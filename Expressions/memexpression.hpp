b0035451057bd4c24a4207065c3277a9b2ea1e41
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-04-09 11:22:34 -0600

    Added function declarations for regexpression and memexpression

--- Expressions/memexpression.hpp
+++ Expressions/memexpression.hpp
@@ -7,6 +7,18 @@ class MemExpression : public Expression{
 public:
     MemExpression();
     Expression* add(Expression* other);
+    Expression* sub(Expression* other);
+    Expression* mult(Expression* other);
+    Expression* div(Expression* other);
+    Expression* mod(Expression* other);
+    Expression* lt(Expression* other);
+    Expression* lte(Expression* other);
+    Expression* gt(Expression* other);
+    Expression* gte(Expression* other);
+    Expression* ne(Expression* other);
+    Expression* eq(Expression* other);
+    Expression* andfun(Expression* other);
+    Expression* orfun(Expression* other);
 private:
     std::string reg;
     int offset;
--- Expressions/regexpression.hpp
+++ Expressions/regexpression.hpp
@@ -3,6 +3,18 @@ public:
     RegExpression();
     RegExpression(ConstExpression c);
     Expression* add(Expression* other);
+    Expression* sub(Expression* other);
+    Expression* mult(Expression* other);
+    Expression* div(Expression* other);
+    Expression* mod(Expression* other);
+    Expression* lt(Expression* other);
+    Expression* lte(Expression* other);
+    Expression* gt(Expression* other);
+    Expression* gte(Expression* other);
+    Expression* ne(Expression* other);
+    Expression* eq(Expression* other);
+    Expression* andfun(Expression* other);
+    Expression* orfun(Expression* other);
 private:
     std::string reg;
 };
