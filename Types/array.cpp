d9e1885a73d25f20de6724fa5978a33e9dd2c803
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-04-12 21:56:47 -0600

    Array class built. Now learning how to impliment

--- Types/array.cpp
+++ Types/array.cpp
@@ -1,1 +1,34 @@
 #include "array.hpp"
+
+Array::Array(int l, int h, Type* tprm){
+    low = l;
+    hi = h;
+    t = tprm;
+}
+
+Array::Array(int l, int h){
+    low = l;
+    hi = h;
+    t = new Integer();
+}
+
+Array::Array(int l, int h, std::string tstring){
+    low = l;
+    hi = h;
+    if(tstring == "integer"){
+        t = new Integer();
+    }
+    else if(tstring == "char"){
+        t = new Char();
+    }
+    else if(tstring == "string"){
+        t = new String();
+    }
+    else if(tstring == "boolean"){
+        t = new Boolean();
+    }
+}
+
+Type* Array::getCopyPtr(){
+    return new Array(low, hi);
+}
--- Types/array.hpp
+++ Types/array.hpp
@@ -2,13 +2,23 @@
 #define ARRAY_HPP
 
 #include "type.hpp"
+#include "simpletype.hpp"
 #include "../Expressions/expression.hpp"
 #include "../Expressions/memexpression.hpp"
 
-class Array : public Type{
+class Array : public Type {
+public:
+    Array(int, int);
+    Array(int, int, Type*);
+    Array(int, int, std::string);
+    Type* getCopyPtr();
+    int size() { return hi - low + 1; }
+    std::string getTypeAsString() { return "array"; }
+
+    int getOffset(int);
 private:
     Type* t;
-    int lowbound, upbound;
+    int low, hi;
 };
 
 #endif
